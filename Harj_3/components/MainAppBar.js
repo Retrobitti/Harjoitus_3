eafdd82a406cce181920bf0135638f34df32d67f
Retrobitti <PekkaParviainen@protonmail.com>
2024-01-22 13:12:31 +0200

    Valamis

---
+++ Harj_3/components/MainAppBar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {Appbar} from "react-native-paper";
+
+export default function MainAppBar(props){
+    return(
+        <Appbar.Header
+         mode="center-aligned"
+         elevated={true}
+         >
+            <Appbar.Content title={props.title}></Appbar.Content>
+         </Appbar.Header>
+    )
+    
+}
